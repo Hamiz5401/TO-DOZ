a9a71541338bdc2222a529d0ceeccd022724db3a
kuikaopong <kuikaopong.2002@gmail.com>
2022-10-11 23:43:57 +0700

    default urls

--- To_DoZ/urls.py
+++ To_DoZ/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.main, name='main'),
+    path('', views.home, name='home'),
+    path('history/', views.history, name='history'),
+    path('detail/<int:pk_list>/<int:pk_task>', views.detail, name='detail'),
 ]
