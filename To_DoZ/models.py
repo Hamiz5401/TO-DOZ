9e56b0b026d3c67ad72cf911c71a1fc358df15b4
kuikaopong <kuikaopong.2002@gmail.com>
2022-10-11 23:43:42 +0700

    default models

--- To_DoZ/models.py
+++ To_DoZ/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+
+class ToDoList(models.Model):
+    """List Model."""
+    subject_text = models.CharField(max_length=200)
+    classroom_API = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+
+class History(models.Model):
+    to_do_list = models.ForeignKey(ToDoList, on_delete=models.CASCADE)
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    detail = models.CharField(max_length=1000)
+    priority = models.BooleanField(default=False)
+    status = models.BooleanField(default=False)
+    deadline = models.DateTimeField(null=True, blank=True)
+    to_do_list = models.ForeignKey(ToDoList, on_delete=models.CASCADE)
+
