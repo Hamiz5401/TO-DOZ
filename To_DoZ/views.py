a3f7df51e5d927a61ed9b10b67c5f24f1e77f395
Kittiporn Theamnooch <kittiporn132@gmail.com>
2022-10-25 00:48:31 +0700

    Google classroom API but a lot of bugs rn

--- To_DoZ/views.py
+++ To_DoZ/views.py
@@ -10,7 +10,60 @@ from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from .models import ToDoList, Task
 
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+import json
 import os.path
+
+SCOPES = [
+    'https://www.googleapis.com/auth/classroom.courses.readonly',
+    'https://www.googleapis.com/auth/classroom.coursework.me'
+]
+
+creds = None
+# The file token.json stores the user's access and refresh tokens, and is
+# created automatically when the authorization flow completes for the first
+# time.
+if os.path.exists('token.json'):
+    creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+# If there are no (valid) credentials available, let the user log in.
+if not creds or not creds.valid:
+    if creds and creds.expired and creds.refresh_token:
+        creds.refresh(Request())
+    else:
+        flow = InstalledAppFlow.from_client_secrets_file(
+            'To_DoZ/credentials.json', SCOPES)
+        creds = flow.run_local_server(port=0)
+    # Save the credentials for the next run
+    with open('token.json', 'w') as token:
+        token.write(creds.to_json())
+
+try:
+    service = build('classroom', 'v1', credentials=creds)
+
+    # Call the Classroom API
+    results = service.courses().list(pageSize=10).execute()
+    courses = results.get('courses', [])
+
+    # classwork = service.courses.courseWork.list().execute
+
+    if not courses:
+        print('No courses found.')
+    # Prints the names of the first 10 courses.
+    classroom_data = []
+    for course in courses:
+        classwork = service.courses().courseWork().list(courseId=course['id']).execute()
+        classroom_data.append(course)
+
+    with open("google_classroom.json", "a") as outfile:
+        json.dump(classroom_data, outfile, indent=4)
+
+except HttpError as error:
+    print('An error occurred: %s' % error)
 
 
 @method_decorator(login_required, name="dispatch")
@@ -20,6 +73,20 @@ class HomeView(generic.ListView):
 
     def get_queryset(self):
         user = self.request.user
+        data = open('google_classroom.json', "r")
+        classroom_data_json = json.load(data)
+        for g_data in classroom_data_json:
+            g_classroom_todo = ToDoList.objects.create(user=user, subject=g_data['name'])
+            # print(classwork)
+            try:
+                for g_task in classwork['courseWork']:
+                    print(g_task)
+                    # Task.objects.create(title=g_task['title'],
+                    #                     detail=g_task['description'],
+                    #                     # deadline will be added later
+                    #                     to_do_list=g_classroom_todo)
+            except KeyError:
+                print(f"    No classwork")
         return ToDoList.objects.filter(user=user)
 
 
