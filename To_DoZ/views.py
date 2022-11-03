ffce31784a17679b423bc61034e6ce9cf875882c
Hamiz5401 <kittiporn132@gmail.com>
2022-11-03 13:46:49 +0700

    Fix detail page entry error

--- To_DoZ/views.py
+++ To_DoZ/views.py
@@ -133,7 +133,6 @@ class DetailView(generic.DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        print(context)
         # context['task'] = self.kwargs['task']
         return context
 
