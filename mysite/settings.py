b82b380e460c77d630ba27210f3a2b5337e1290b
Phukit-Time <phukit.s@ku.th>
2022-11-23 16:47:42 +0700

    deploy 23/11/4:47

--- mysite/settings.py
+++ mysite/settings.py
@@ -60,7 +60,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'mysite.urls'
