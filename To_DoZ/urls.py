413c7a1187a53901f345d72601589f267731ed4e
Phukit-Time <phukit.s@ku.th>
2022-11-23 16:43:40 +0700

    23/11/4:43

--- To_DoZ/urls.py
+++ To_DoZ/urls.py
@@ -8,15 +8,15 @@ urlpatterns = [
     path('table/', views.TableView.as_view(), name='table'),
     path('history/', views.HistoryView.as_view(), name='history'),
     path('detail/<int:pk_list>/<int:pk>', views.DetailView.as_view(), name='detail'),
-    
+
     path('create', views.ListCreateView.as_view(), name='create_list'),
     path('<int:pk>/update', views.ListUpdateView.as_view(), name='update_list'),
     path('<int:pk>/delete', views.ListDeleteView.as_view(), name='delete_list'),
-    
+
     path('detail/<int:pk_list>/create', views.TaskCreateView.as_view(), name='create_task'),
     path('detail/<int:pk_list>/<int:pk>/update', views.TaskUpdateView.as_view(), name='update_task'),
     path('detail/<int:pk_list>/<int:pk>/delete', views.TaskDeleteView.as_view(), name='delete_task'),
-    
+
     path('detail/<int:pk_task>/done', views.done, name='done'),
     path('get_classroom_data', views.add_classroom, name='get_data'),
 
