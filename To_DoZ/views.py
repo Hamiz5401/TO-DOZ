8f109d38b7877c572c21a1853de5c2224f72ba61
Phukit-Time <phukit.s@ku.th>
2022-11-29 21:53:56 +0700

    deploy 29/11/9:53

--- To_DoZ/views.py
+++ To_DoZ/views.py
@@ -252,7 +252,11 @@ def create_classroom_data(request):
 
     # Store the Credential.
     if not Google_token.objects.filter(user=user).exists():
-        creds = create_credential(creds, request)
+        try:
+            creds = create_credential(creds, request)
+        except:
+            return HttpResponseRedirect(reverse("To_DoZ:home"))
+
         Google_token.objects.create(user=user,
                                     token=creds.token,
                                     refresh_token=creds.refresh_token,
